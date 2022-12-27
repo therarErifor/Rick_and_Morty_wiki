315363fba3c6bebd55f93b6fb36443c1970fc5f4
therarErifor <therar.erifor@gmail.com>
2022-12-27 22:21:54 +0500

    commit

--- main/lib/main.dart
+++ main/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Rick and Morty Wiki',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
