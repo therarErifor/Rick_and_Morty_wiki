92f2ec9f5ce112c7f75aefe2818499f11845121f
therarErifor <therar.erifor@gmail.com>
2022-12-27 21:42:50 +0500

    initial commit

--- main/lib/main.dart
+++ main/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Rick and Morty wiki'),
+      home: const MyHomePage(title: 'Rick and Morty Wiki'),
     );
   }
 }
