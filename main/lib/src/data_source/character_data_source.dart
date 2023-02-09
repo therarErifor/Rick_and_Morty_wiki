b4d054a2d4b0c02448dc17f9f56be3f773fa0375
therarErifor <therar.erifor@gmail.com>
2023-02-10 00:41:21 +0500

    delete a trash

--- main/lib/src/data_source/character_data_source.dart
+++ main/lib/src/data_source/character_data_source.dart
@@ -1,19 +1,9 @@
-
-
-import 'dto/info_dto.dart';
-
 import 'dto/character_detailed_dto.dart';
-import 'dto/character_dto.dart';
+
 import 'dto/page_dto.dart';
 
-abstract class CharacterDataSource{
+abstract class CharacterDataSource {
   Future<PageDto> getPageAsync(int pageNumber);
+
   Future<CharacterDetailedDto> getCharacterDetailedAsync(int id);
-  // Future<InfoDto> getInfoPagesAsync();
-
-
-  //Future<InfoDto> getInfoAsync();
 }
-
-
-
